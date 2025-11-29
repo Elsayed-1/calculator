2849a877b07bff4b00744f5060266cb1f1ce7a98
Elsayed Samy <elsayed1122333@gmail.com>
2025-11-29 15:51:20 +0200

    Initial upload for calculator project

---
+++ script.js
@@ -0,0 +1,38 @@
+const Allbody=document.querySelector(".Allproject")
+const Display=document.querySelector(".display")
+
+Allbody.addEventListener('click',(e)=>{
+    if(e.target.nodeName=='BUTTON'){
+        switch(e.target.textContent){
+            case 'C':
+            clear()
+             break;
+           case 'DEL':
+              remove()
+              break
+              case "=":
+                evaluate()
+                break
+            default :
+            Addcontent(e.target.textContent)
+        }
+    }
+})
+
+function clear(){
+    Display.textContent=""
+}
+function Addcontent(value){
+Display.textContent=Display.textContent+value
+}
+
+function remove(){
+   currentvalue  = Display.textContent  
+
+    Display.textContent=currentvalue.substring(0,currentvalue.length -1)
+
+}
+function evaluate(){
+    let calc=math.evaluate(Display.textContent)
+    Display.textContent=calc
+}
